fc1cf147e0ca6e29183b0ebd7bc43ae73a2ed5b9
Kingfish Threesevens <scott.malec@uth.tmc.edu>
2014-12-16 14:01:51 -0600

    Create rPythonExample.r

---
+++ rPythonExample.r
@@ -0,0 +1,6 @@
+# install.packages("rPython", repos = "http://www.datanalytics.com/R")
+# if this doesn't work, go to cran site and download an older version as tar.gz and install it through rstudio 
+# from local drive
+
+library(rPython)
+
