e2291d463d30cdd731c888ebc7ff44a1fa2603e2
Kingfish Threesevens <scott.malec@uth.tmc.edu>
2014-12-16 14:25:38 -0600

    Create posTaggeRpy2.r

---
+++ posTaggeRpy2.r
@@ -0,0 +1,21 @@
+# convert to R
+
+import nltk
+import sys
+from nltk.tokenize import RegexpTokenizer
+
+# sentence = "I am the walrus. I am the eggman. The man eats the apple. Ivan shot the bear."
+for arg in sys.argv: 
+    print arg
+sentences = nltk.sent_tokenize(arg)
+print(sentences)
+tokenizer = RegexpTokenizer(r'\w+')
+
+for sentence in sentences: 
+ #   tokens = nltk.word_tokenize(sentence) 
+ #   print(tokens)
+    tokens = tokenizer.tokenize(sentence)
+    tagged = nltk.pos_tag(tokens)
+    print(tagged)
+    entities = nltk.ne_chunk(tagged)
+    print(entities)
