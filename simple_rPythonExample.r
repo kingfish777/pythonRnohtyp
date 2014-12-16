a13c7d49e668aa92c297609133c135b478abd3fb
Kingfish Threesevens <scott.malec@uth.tmc.edu>
2014-12-16 16:24:45 -0600

    Update simple_rPythonExample.r

--- simple_rPythonExample.r
+++ simple_rPythonExample.r
@@ -11,8 +11,8 @@ python.exec("def happyBirthdayToYou(): print(\"happy birthday to you!\")")
 python.exec("happyBirthdayToYou()")
 
 # unfortunately, rPython does not work on Wintel systems. However, despair not, you can escape to the OS with system()
- ##system('python -c "def happyBirthdayToYou():  print(\"happy birthday to you!\")"')
- ##system('python -c "happyBirthdayToYou()"')
+  ##system('python -c "def happyBirthdayToYou():  print(\"happy birthday to you!\")"')
+  ##system('python -c "happyBirthdayToYou()"')
        #### NOT WORKING!!! OUTTA LUCK w\ Windows! 
   # use python.exec when you only need to execute or evaluate an expression within Python
   # use python.get to retrieve an object post-execution
@@ -23,19 +23,17 @@ python.exec("import nltk.corpus")
 python.exec("from nltk.corpus import PlaintextCorpusReader")
 python.exec("from urllib import urlopen")
 python.exec("import numpy")
-#python.exec("corpus_root = '/amanuensis/data/raw_data/corpora/fedchairs/gReenspanCorpus/'")
-#python.exec("corpus = PlaintextCorpusReader(corpus_root, '.*')")
-#python.get("corpus.fileids()")
-#python.get("corpus.open('greenspan_19990506.txt')")
-python.exec("url = 'greenspan_19990506.txt'")
+python.exec("url = '/amanuensis/data/raw_data/corpora/fedchairs/gReenspanCorpus/greenspan_19980828.txt'")
 python.exec("raw = open(url).read()")
 python.exec("ttt = nltk.tokenize.TextTilingTokenizer(w=7, k=3, smoothing_width=6, smoothing_rounds=10)")
 python.exec("tiles = ttt.tokenize(raw)")
 python.exec("print(tiles[1])")
-firstTextTile <- python.get("textTiles[-1]")
+firstTextTile <- python.get("textTiles[1]")
 firstTextTile
 python.get("firstTextTile")
-text <- paste("espeak -v+whisper -k20 -p 79 -s 225 -f \"", firstTextTile, "\"", sep="")
+text <- paste("espeak -v+whisper -k20 -p 79 -s 225 \"", firstTextTile, "\"", sep="")
 text
 system(text)
+
+
 
